f2d4cf4826ab08502572e80589c4e521d2713b9b
rishitJ <121947502+rishitJ@users.noreply.github.com>
2023-10-11 19:44:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function setup() 
+{
+    canvas = createCanvs(400,300)
+    canvas.create()
+}
+function preload() 
+{
+    
+}
+function draw() 
+{
+    
+}
+function take_snapshot() 
+{
+    save("myjokerimg.png")
+}
